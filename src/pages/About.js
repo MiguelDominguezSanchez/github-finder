9b576e589f5223a90f214ec5a424b83ca2f2a5d2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-02-18 19:58:23 +0100

    Pages & Routes

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+function About() {
+	return (
+		<div>
+			<h1 className='text-6xl-mb-4'>Github Finder</h1>
+			<p className='mb-4 text-2xl font-light'>
+				A React app to search Github profiles and see profile details. This
+				project is part of the <a href='#'>React Front To Back</a>Lorem Ipsum
+				Dolor
+				<strong>
+					<a href='#'>sit amet</a>
+				</strong>
+				.
+			</p>
+
+			<p className='text-lg text-gray-400'>
+				Version <span className='text-white'>1.0.0</span>
+			</p>
+
+			<p className='text-lg text-gray-400'>
+				Layout By:
+				<a className='text-white' href='#'>
+					{' '}
+					lorem ipsum
+				</a>
+			</p>
+		</div>
+	)
+}
+
+export default About
